daa80d516e28eff2074bd8ce6032760523a9f946
Jeremy Petch <jeremy.petch@outlook.com>
2025-02-05 18:05:47 +0000

    Fix some warnings & errors

--- QIO/QIORandom.hs
+++ QIO/QIORandom.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 -- | This module implements various functions that return a probabilistic result, 
 -- defined as unitary operators, and quantum computations.
@@ -11,16 +12,15 @@ import Data.Complex
 
 -- | The exponentiated Pauli-X rotation
 rX :: RR -> Rotation
-rX r (x,y) = if x==y then (cos (r/2):+0) else (0:+ (-(sin (r/2))))
+rX r (x,y) = if x==y then cos (r/2):+0 else 0:+ (-(sin (r/2)))
 
 -- | The exponentiated Pauli-Y rotation
 rY :: RR -> Rotation
-rY r (x,y) = if x==y then (cos (r/2):+0) else (s * sin (r/2):+0) where s = if x then 1 else -1
+rY r (x,y) = if x==y then cos (r/2):+0 else s * sin (r/2):+0 where s = if x then 1 else -1
 
 -- | Applies a Hadamard rotation to each qubit in the given list of qubits
 hadamards :: [Qbit] -> U
-hadamards [] = mempty
-hadamards (q:qs) = uhad q `mappend` hadamards qs
+hadamards = foldr (mappend . uhad) mempty
 
 -- | returns the highest integer power of 2 that is less than or equal to \x\
 pow2 :: Int -> Int
@@ -31,8 +31,8 @@ pow2 x = pow2' 0
 -- | A rotation that, given a qubit in state 0, leaves it in a super-position of
 -- 0 and 1, such that the probability of measuring as state 0 is \ps\.
 weightedU :: RR -> Qbit -> U
-weightedU ps q | sqrt ps <= 1 = rot q (rX (2*(acos (sqrt ps))))
-           | otherwise = error ("weightedU: Invalid Probability: " ++ show ps)      
+weightedU ps q | sqrt ps <= 1 = rot q (rX (2*acos (sqrt ps)))
+           | otherwise = error ("weightedU: Invalid Probability: " ++ show ps)
 -- | A QIO computation that uses the "weightedU" unitary, to return a Bool that
 -- has a probablity of \pf\ of being False.
 weightedBool :: RR -> QIO Bool
@@ -47,18 +47,19 @@ rlf bs = bs
 
 -- | removes any leading Falses from the (big-endian) bit-wise representation
 -- of the given Int.
-rlf_l :: Int -> [Bool]
-rlf_l x = rlf (reverse (int2bits x))
+rlfL :: Int -> [Bool]
+rlfL x = rlf (reverse (int2bits x))
 
 -- | returns the number of bits left after calling the "flf_l" function
-rlf_n :: Int -> Int
-rlf_n x = length (rlf_l x)
+rlfN :: Int -> Int
+rlfN x = length (rlfL x)
 
 -- | Given an Int \max\ that is the largest number required to be represented in
 -- a quantum register, this function trims the front off the given register, to
 -- leave the number of qubits required to represent \max\.
 trim :: Int -> [Qbit] -> [Qbit]
-trim max qbs = drop ((length qbs)-(rlf_n max)) qbs
+trim max qbs = drop (length qbs-rlfN max) qbs
+
 
 -- | Given an Int \max\, and a quantum register in the state \max\, this function 
 -- defines a unitary operation that will leave the quantum register in state that
@@ -68,16 +69,16 @@ randomU max qbs = randomU' max (trim max qbs)
  where
   randomU' _ [] = mempty
   randomU' 0 _ = mempty
-  randomU' max (q:qbs) = weightedU (fromIntegral ((max+1)-p)/fromIntegral (max+1)) q
+  randomU' max (q:qbs) = weightedU (fromIntegral (max+1-p)/fromIntegral (max+1)) q
                  `mappend`
-                 condQ q (\x -> if x then (randomU (max-p) qbs) 
-                                else (hadamards qbs))
+                 condQ q (\x -> if x then randomU (max-p) qbs
+                                else hadamards qbs)
                   where p = pow2 max
 
 -- | A quantum computation that will return a quantum integer in a state that
 -- has equal probabilities of being measured in any of the state 0 to \max\.
 randomQInt :: Int -> QIO QInt
-randomQInt max = do 
+randomQInt max = do
   qbs <- mkQ (reverse (int2bits max))
   applyU (randomU max qbs)
   return (QInt (reverse qbs))
@@ -90,7 +91,7 @@ randomQIO (min,max) = do q <- randomInt (max-min)
 
 -- | A quantum computation that measures the outcome of "randomQInt"
 randomInt :: Int -> QIO Int
-randomInt max = do 
+randomInt max = do
   q <- randomQInt max
   measQ q
 
@@ -98,40 +99,40 @@ randomInt max = do
 -- any number in the range 0 to \x\-1
 random :: Int -> QIO Int
 random x = randomInt (x-1)
-            
+
 -- | This function uses a Quantum computation to simulate the roll of a dice
 dice :: IO Int
-dice = do 
+dice = do
   x <- run (randomInt 5)
   return (x+1)
 
 -- | This function simulates the given number of repitions of dice rolls
-dice_rolls :: Int -> IO [Int]
-dice_rolls 0 = return []
-dice_rolls y = do 
+diceRolls :: Int -> IO [Int]
+diceRolls 0 = return []
+diceRolls y = do
   x <- dice
-  xs <- dice_rolls (y-1)
+  xs <- diceRolls (y-1)
   return (x:xs)
 
 -- | Returns the number of occurences of 1 through 6 in the given list of Ints
 occs :: [Int] -> (Int,Int,Int,Int,Int,Int)
 occs rs = (rs' 1,rs' 2,rs' 3,rs' 4,rs' 5,rs' 6)
-  where 
+  where
    rs' x = length ([y|y<-rs,y==x])
 
 -- | Returns the number of occurences of 1 through 6 in the given number of
 -- rolls of the dice.
 probs' :: Int -> IO (Int,Int,Int,Int,Int,Int)
-probs' x = do 
-  xs <- dice_rolls x
+probs' x = do
+  xs <- diceRolls x
   return (occs xs)
 
 -- | Returns the percentage of occurences of 1 through 6, after the given number
 -- of rolls of the dice.
 probs :: Int -> IO (RR,RR,RR,RR,RR,RR)
-probs x = do 
+probs x = do
   (a,b,c,d,e,f) <- probs' x
   return (fromIntegral a/x',fromIntegral b/x',fromIntegral c/x',fromIntegral d/x',fromIntegral e/x',fromIntegral f/x')
     where x' = fromIntegral x
 
-           
+
--- QIO/Qft.hs
+++ QIO/Qft.hs
@@ -1,12 +1,11 @@
-
 -- | This module provides an implementation of the Quantum Fourier Transform
 -- in QIO.
 module QIO.Qft where
 
 import Data.Monoid as Monoid
-import QIO.QioSyn
-import QIO.Qio
 import QIO.Qdata
+import QIO.Qio
+import QIO.QioSyn
 
 -- | Defines the unitary the represents appliying a Quantum Fourier Transform
 -- to the given quantum register.
@@ -17,29 +16,31 @@ qft qs = condQ qs (\bs -> qftAcu qs bs [])
 -- apply smaller QFTs to the tail of the given quantum register.
 qftAcu :: [Qbit] -> [Bool] -> [Bool] -> U
 qftAcu [] [] _ = mempty
-qftAcu (q:qs) (b:bs) cs = qftBase cs q `mappend` qftAcu qs bs (b:cs)
+qftAcu (q : qs) (b : bs) cs = qftBase cs q `mappend` qftAcu qs bs (b : cs)
 
 -- | The \"base\" step involved in a QFT is a series of controlled rotations.
 qftBase :: [Bool] -> Qbit -> U
-qftBase bs q =  f' bs q 2
-    where f' [] q _ = uhad q
-          f' (b:bs) q x = if b then (rotK x q) `mappend` f' bs q (x+1) 
-                  else f' bs q (x+1)
+qftBase bs q = f' bs q 2
+  where
+    f' [] q _ = uhad q
+    f' (b : bs) q x =
+      if b
+        then rotK x q `mappend` f' bs q (x + 1)
+        else f' bs q (x + 1)
 
---need to change this into a conQRec???
+-- need to change this into a conQRec???
 -- e.g. qft [Qbit 0]
 -- = condQ [Qbit 0] (\(b:bs) -> uhad 0 `mappend` mempty)
 -- but gives  cond 0 (\x -> if x then uhad 0 else uhad 0) which is forbidden
 
--- | The rotation used in the QFT is a phase rotation, parameterised by the 
+-- | The rotation used in the QFT is a phase rotation, parameterised by the
 -- angle 1/(2^\k\)
 rotK :: Int -> Qbit -> U
-rotK k q = uphase q (1.0/(2.0^k))
+rotK k q = uphase q (1.0 / 2.0 ^ k)
 
 -- | A test of the QFT unitary, over a quantum integer initialised to \n\.
 tryQft :: Int -> QIO Int
-tryQft n = do 
+tryQft n = do
   QInt qs <- mkQ n
-  applyU(qft qs)
-  x <- measQ (QInt qs)
-  return x
+  applyU (qft qs)
+  measQ (QInt qs)
